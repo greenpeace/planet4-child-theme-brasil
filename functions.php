4ff085f1a1c3111d079dbd6aa0bfa8bf88b6e746
Pieter Vincent <pvincent@greenpeace.org>
2022-04-25 15:18:39 +0200

    Fix allowed blocks filter
    This used the deprecated version (without `_all`). Not sure why this is still in the child themes if it doesn't work :thinking: 
    
    Also add Post Excerpt block.

--- functions.php
+++ functions.php
@@ -14,11 +14,11 @@ function enqueue_child_styles() {
 /** 
 * Allows the loop query block usage
 */
-add_filter( 'allowed_block_types', 'p4_brasil_allowed_blocks', 11, 2 );
+add_filter( 'allowed_block_types_all', 'p4_brasil_allowed_blocks', 11, 2 );
 
-function p4_brasil_allowed_blocks( $allowed_blocks, $post ) {
-    if ( $post->post_type === 'page' ) {
-        $allowed_blocks = array_merge( $allowed_blocks, ['core/query-loop', 'core/post-featured-image']);
+function p4_brasil_allowed_blocks( $allowed_blocks, $context ) {
+    if ( $context->post && $context->post->post_type === 'page' ) {
+        $allowed_blocks = array_merge( $allowed_blocks, ['core/query-loop', 'core/post-featured-image', 'core/post-excerpt']);
     };
     return $allowed_blocks;
 }
