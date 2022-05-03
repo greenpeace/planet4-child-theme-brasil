96a8f8b83c15f08224581bfea8d9cfc1837e4174
Rickson Figueira <sadrisco@outlook.com>
2022-05-03 10:33:24 -0300

    Updated styles.css and functions.php
    Small css fixes and indentation
    Added a function to allow Dashicons load for non-logged users

--- functions.php
+++ functions.php
@@ -22,3 +22,15 @@ function p4_brasil_allowed_blocks( $allowed_blocks, $context ) {
     };
     return $allowed_blocks;
 }
+
+/**
+ * Allows the Dashicons for non-logged users
+ */
+
+add_action( 'wp_enqueue_scripts', 'dashicons_front_end' );
+
+function dashicons_front_end() {
+
+   wp_enqueue_style( 'dashicons' );
+
+}
