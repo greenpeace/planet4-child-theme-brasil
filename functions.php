227df1495e6f9ede4add62902013ba28fc43c20f
Rickson Figueira <sadrisco@outlook.com>
2022-10-13 12:08:36 -0300

    added birth mask at proteja o oceano

--- functions.php
+++ functions.php
@@ -203,3 +203,33 @@ add_filter( 'gform_field_validation_10_8', function ( $result, $value, $form, $f
 }, 10, 4 );
 
 add_filter( 'gform_confirmation', 'gform_confirmation', 10, 4 );
+
+/**
+ *  Proteja o oceano - Check if user is underage at Gravity Forms birthdate input
+ */
+$field = GFFormsModel::get_field( $form, 1 );
+$date_type = $field->dateType;
+
+add_filter( 'gform_field_validation_13_8', function ( $result, $value, $form, $field ) {
+    if ( $result['is_valid'] ) {
+        if ( is_array( $value ) ) {
+            $value = array_values( $value );
+        }
+        $date_value = GFFormsModel::prepare_date( $field->dateFormat, $value );
+        $originalDate = $date_value;
+        $newDate = date("d-m-Y", strtotime($originalDate));
+
+        $today = new DateTime();
+        $diff  = $today->diff( new DateTime( $newDate ) );
+        $age   = $diff->y;
+  
+        if ( $age < 18 ) {
+            $result['is_valid'] = false;
+            $result['message']  = 'Desculpe, apenas maiores de 18 anos podem assinar.';
+        }
+    }
+
+    return $result;
+}, 10, 4 );
+
+add_filter( 'gform_confirmation', 'gform_confirmation', 10, 4 );
