7876ddaa65fa6a9b1b789f3ba5cdf2ed87228667
Rickson Figueira <sadrisco@outlook.com>
2022-12-12 15:32:25 -0300

    Syntax updates

--- includes/wordpress-allows-loop-query-block.php
+++ includes/wordpress-allows-loop-query-block.php
@@ -4,11 +4,9 @@
  */
 add_filter('allowed_block_types_all', 'p4_brasil_allowed_blocks', 11, 2);
 
-function p4_brasil_allowed_blocks($allowed_blocks, $context)
-{
+function p4_brasil_allowed_blocks($allowed_blocks, $context) {
     if ($context->post && $context->post->post_type === 'page') {
         $allowed_blocks = array_merge($allowed_blocks, ['core/query', 'core/post-featured-image', 'core/post-excerpt']);
-    }
-    ;
+    };
     return $allowed_blocks;
 }
