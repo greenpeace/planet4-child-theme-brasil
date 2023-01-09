8283b4ca4644e4975ef52554cb5ea45efdfde4c2
Rickson Figueira <sadrisco@outlook.com>
2023-01-09 16:20:07 -0300

    Removed front-end params display

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 require_once( 'includes/load-styles-from-child-theme.php' );
 require_once( 'includes/gravity-forms/gravity-forms-extensions.php' );
 require_once( 'includes/wordpress-allows-loop-query-block.php' );
-require_once( 'includes/displays-url-params-on-frontend.php' );
+// require_once( 'includes/displays-url-params-on-frontend.php' );
 require_once( 'includes/jquery-cdn-import.php' );
 // require_once( 'includes/handtalk-scripts.php' );
 // require_once( 'includes/csp-headers.php' );
