da54e7959ccbac6510f3827429d0763a08f9c6cf
Rickson Figueira <sadrisco@outlook.com>
2023-01-11 11:01:32 -0300

    Updated jQuery load priority

--- includes/jquery-cdn-import.php
+++ includes/jquery-cdn-import.php
@@ -2,6 +2,6 @@
 
 function gpbr_add_jquery() {
 	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), null, true);
+	wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), null, false);
 }
-add_action('wp_enqueue_scripts', 'gpbr_add_jquery');
+add_action('wp_enqueue_scripts', 'gpbr_add_jquery', 1);
