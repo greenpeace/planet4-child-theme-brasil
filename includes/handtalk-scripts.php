409bba33abc75551f2ba986e2fffc88210783782
Rickson Figueira <sadrisco@outlook.com>
2023-01-19 14:29:58 -0300

    Added Maya as Handtalk Avatar

--- includes/handtalk-scripts.php
+++ includes/handtalk-scripts.php
@@ -8,7 +8,10 @@ function gpbr_add_handtalk_script()
     <script src="https://plugin.handtalk.me/web/latest/handtalk.min.js"></script>
     <script>
         var ht = new HT({
-            token: "40820ef34c22e3e84ffb669c7e31258d"
+        token: "40820ef34c22e3e84ffb669c7e31258d",
+        pageSpeech: true,
+        avatar: "MAYA",
+        maxTexSize: 1000  
         });
     </script>
     <?php
