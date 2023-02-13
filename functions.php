06906d871dfa62965c0f270ee79c8a2de39d9643
Rickson Figueira <sadrisco@outlook.com>
2023-02-13 10:59:09 -0300

    Added birth mask on Garimpo petition

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 require_once( 'includes/load-styles-from-child-theme.php' );
 require_once( 'includes/gravity-forms/gravity-forms-extensions.php' );
 require_once( 'includes/wordpress-allows-loop-query-block.php' );
-// require_once( 'includes/displays-url-params-on-frontend.php' );
+require_once( 'includes/displays-url-params-on-frontend.php' );
 require_once( 'includes/jquery-cdn-import.php' );
 require_once( 'includes/handtalk-scripts.php' );
 require_once( 'includes/csp-headers.php' );
@@ -20,3 +20,4 @@ require_once( 'includes/csp-headers.php' );
 require_once( 'includes/petitions/manifesto-pela-agroecologia.php' );
 require_once( 'includes/petitions/proteja-os-oceanos.php' );
 require_once( 'includes/petitions/todos-pela-amazonia.php' );
+require_once( 'includes/amazonia-livre-do-garimpo.php' );
