6d672c7ed6eac1271b292fb71f72acd90bdfc116
Rickson Figueira <sadrisco@outlook.com>
2023-04-13 17:47:20 -0300

    Enabled audima plugin

---
+++ includes/audima.php
@@ -0,0 +1,10 @@
+<?php
+
+function add_audima_after_post_title($title) {
+    if (is_single() && in_the_loop() && is_main_query()) { // check if it's a blog post
+        $title .= '<div id=\'audimaWidget\'></div><script src=\'https://audio4.audima.co/audima-widget.js\'></script>
+';
+    }
+    return $title;
+}
+add_filter('the_title', 'add_audima_after_post_title');
