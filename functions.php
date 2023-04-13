6f03d67efb773af10bc36dcb8ccc295e6cb52b0f
Rickson Figueira <sadrisco@outlook.com>
2023-04-13 17:46:44 -0300

    Added age validator for new petitions

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@ require_once( 'includes/jquery-cdn-import.php' );
 require_once( 'includes/handtalk-scripts.php' );
 require_once( 'includes/csp-headers.php' );
 
+// require_once ('includes/audima.php') ;
+
 
 /**
  * Petition includes
@@ -21,3 +23,5 @@ require_once( 'includes/petitions/manifesto-pela-agroecologia.php' );
 require_once( 'includes/petitions/proteja-os-oceanos.php' );
 require_once( 'includes/petitions/todos-pela-amazonia.php' );
 require_once( 'includes/petitions/amazonia-livre-do-garimpo.php' );
+require_once( 'includes/petitions/futuro-livre-de-plasticos.php' );
+require_once( 'includes/petitions/basta-de-tragedias-chuvas.php' );
