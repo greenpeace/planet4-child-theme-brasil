1d63e619de1789241546eb9c93f8cf5cb645b85e
Rickson Figueira <sadrisco@outlook.com>
2023-04-13 17:48:29 -0300

    Syntax fix

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ require_once( 'includes/jquery-cdn-import.php' );
 require_once( 'includes/handtalk-scripts.php' );
 require_once( 'includes/csp-headers.php' );
 
-// require_once ('includes/audima.php') ;
+require_once ('includes/audima.php') ;
 
 
 /**
