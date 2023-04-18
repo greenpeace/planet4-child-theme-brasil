54090499f38e9f447707c0bd6c29abd0b4cd063b
Rickson Figueira <sadrisco@outlook.com>
2023-04-18 16:36:33 -0300

    Changed Audima loading location
    instead of loading the Audima script after the_title hook, i've set the script to load after the share-icons element class using a JS to inject it as a async function.

--- includes/audima.php
+++ includes/audima.php
@@ -1,10 +1,21 @@
 <?php
 
-function add_audima_after_post_title($title) {
+function add_audima_script() {
     if (is_single() && in_the_loop() && is_main_query()) { // check if it's a blog post
-        $title .= '<div id=\'audimaWidget\'></div><script src=\'https://audio4.audima.co/audima-widget.js\'></script>
-';
+        echo '<script async>
+            (function() {
+                var shareButtons = document.querySelector(".share-buttons");
+                if (shareButtons) {
+                    var audimaWidget = document.createElement("div");
+                    audimaWidget.id = "audimaWidget";
+                    shareButtons.parentNode.insertBefore(audimaWidget, shareButtons.nextSibling);
+                    var audimaScript = document.createElement("script");
+                    audimaScript.src = "https://audio4.audima.co/audima-widget.js";
+                    shareButtons.parentNode.insertBefore(audimaScript, shareButtons.nextSibling);
+                }
+            })();
+        </script>';
     }
-    return $title;
 }
-add_filter('the_title', 'add_audima_after_post_title');
+add_action('wp_footer', 'add_audima_script');
+
