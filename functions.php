a939fe7bf46f143e77d23bceb50fd3dc3e9e5577
Rickson Figueira <sadrisco@outlook.com>
2023-05-09 17:22:45 -0300

    Syntax fix

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ require_once( 'includes/displays-url-params-on-frontend.php' );
 require_once( 'includes/jquery-cdn-import.php' );
 require_once( 'includes/handtalk-scripts.php' );
 require_once( 'includes/csp-headers.php' );
-require_once( 'includes/audima.php' );
+// require_once( 'includes/audima.php' );
 // require_once( 'includes/mobile-donate-button-v1.php' );
 
 /**
@@ -21,7 +21,7 @@ require_once( 'includes/audima.php' );
 require_once( 'includes/petitions/manifesto-pela-agroecologia.php' );
 require_once( 'includes/petitions/proteja-os-oceanos.php' );
 require_once( 'includes/petitions/todos-pela-amazonia.php' );
-require_once( 'includes/petitions/amazonia-livre-do-garimpo.php' );
 require_once( 'includes/petitions/futuro-livre-de-plasticos.php' );
 require_once( 'includes/petitions/basta-de-tragedias-chuvas.php' );
-
+require_once( 'includes/petitions/amazonia-livre-de-garimpo.php' );
+require_once( 'includes/petitions/mineracao-em-aguas-profundas.php' );
---
+++ includes/petitions/mineracao-em-aguas-profundas.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *  Todos pela Amazônia - Check if user is underage at Gravity Forms birthdate input
+ */
+$field = GFFormsModel::get_field($form, 1);
+$date_type = $field->dateType;
+
+add_filter('gform_field_validation_9_23', function ($result, $value, $form, $field) {
+    if ($result['is_valid']) {
+        if (is_array($value)) {
+            $value = array_values($value);
+        }
+        $date_value = GFFormsModel::prepare_date($field->dateFormat, $value);
+        $originalDate = $date_value;
+        $newDate = date("d-m-Y", strtotime($originalDate));
+
+        $today = new DateTime();
+        $diff = $today->diff(new DateTime($newDate));
+        $age = $diff->y;
+
+        if ($age < 18) {
+            $result['is_valid'] = false;
+            $result['message'] = 'Desculpe, apenas maiores de 18 anos podem assinar.';
+        }
+    }
+
+    return $result;
+}, 10, 4);
