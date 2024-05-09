147d4803b8e76c506945a9c21c46faa1c12ae496
Rickson Figueira <sadrisco@outlook.com>
2024-05-09 17:23:34 -0300

    updated donation block on blog posts

--- includes/add-custom-block-after-all-blog-posts.php
+++ includes/add-custom-block-after-all-blog-posts.php
@@ -6,7 +6,7 @@
 function adicionar_html_condicional($content) {
     if (is_single() && get_post_type() == 'post') {
         // Adicione HTML para todos os posts de blog ao final do conteúdo
-        $content .= '<div class="has-background gpbr-custom-blog-block"><p>Sem a ajuda de pessoas como você, nosso trabalho não seria possível. O Greenpeace Brasil é uma organização independente - não aceitamos recursos de empresas, governos ou partidos políticos. Por favor, faça uma doação hoje mesmo e nos ajude a ampliar nosso trabalho de pesquisa, monitoramento e denúncia de crimes ambientais. Clique abaixo e faça a diferença!</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a href="https://doe.greenpeace.org.br/emergencia-amazonia/p?entrypoint=p4_customblock" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
+        $content .= '<div class="has-background gpbr-custom-blog-block"><p>O Rio Grande do Sul enfrenta uma tragédia climática sem precedentes, com mais de 1 milhão de pessoas diretamente impactadas pelas fortes chuvas. Nossa campanha está destinando recursos para a compra e entrega de suprimentos emergenciais e apoiando cozinhas solidárias. Precisamos da sua solidariedade nesse momento tão crítico. Clique abaixo e doe agora.</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a target="_blank" href="https://doe.greenpeace.org.br/emergencia-rio-grande-do-sul/p?entrypoint=p4_customblock" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
 
         <style>
         .gpbr-custom-blog-block p {
