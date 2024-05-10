5e7c5e6e2fcbedfb53806d423e9c92e4cf44b7c8
Rickson Figueira <sadrisco@outlook.com>
2024-05-10 15:06:05 -0300

    Update URL on blog posts custom block

--- includes/add-custom-block-after-all-blog-posts.php
+++ includes/add-custom-block-after-all-blog-posts.php
@@ -6,7 +6,7 @@
 function adicionar_html_condicional($content) {
     if (is_single() && get_post_type() == 'post') {
         // Adicione HTML para todos os posts de blog ao final do conteúdo
-        $content .= '<div class="has-background gpbr-custom-blog-block"><p>O Rio Grande do Sul enfrenta uma tragédia climática sem precedentes, com mais de 1 milhão de pessoas diretamente impactadas pelas fortes chuvas. Nossa campanha está destinando recursos para a compra e entrega de suprimentos emergenciais e apoiando cozinhas solidárias. Precisamos da sua solidariedade nesse momento tão crítico. Clique abaixo e doe agora.</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a target="_blank" href="https://doe.greenpeace.org.br/emergencia-rio-grande-do-sul/p?entrypoint=p4_customblock" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
+        $content .= '<div class="has-background gpbr-custom-blog-block"><p>O Rio Grande do Sul enfrenta uma tragédia climática sem precedentes, com mais de 1 milhão de pessoas diretamente impactadas pelas fortes chuvas. Nossa campanha está destinando recursos para a compra e entrega de suprimentos emergenciais e apoiando cozinhas solidárias. Precisamos da sua solidariedade nesse momento tão crítico. Clique abaixo e doe agora.</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a target="_blank" href="https://doe.greenpeace.org.br/emergencia-rio-grande-do-sul/p?appeal=22424&entrypoint=p4_blog" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
 
         <style>
         .gpbr-custom-blog-block p {
