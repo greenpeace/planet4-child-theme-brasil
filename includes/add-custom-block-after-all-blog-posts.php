0378db02bf2d5611e112d41d6ac3f213fbfb8115
Rickson Figueira <sadrisco@outlook.com>
2025-01-15 09:36:26 -0300

    Update the parameter on blog donate block

--- includes/add-custom-block-after-all-blog-posts.php
+++ includes/add-custom-block-after-all-blog-posts.php
@@ -6,7 +6,7 @@
 function adicionar_html_condicional($content) {
     if (is_single() && get_post_type() == 'post') {
         // Adicione HTML para todos os posts de blog ao final do conteúdo
-        $content .= '<div class="has-background gpbr-custom-blog-block"><p>Sem a ajuda de pessoas como você, nosso trabalho não seria possível. O Greenpeace Brasil é uma organização independente - não aceitamos recursos de empresas, governos ou partidos políticos. Por favor, faça uma doação hoje mesmo e nos ajude a ampliar nosso trabalho de pesquisa, monitoramento e denúncia de crimes ambientais. Clique abaixo e faça a diferença!</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a href="https://doe.greenpeace.com.br/?cc=701Pm00000DisbVIAR&entrypoint=blog_donation_block" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
+        $content .= '<div class="has-background gpbr-custom-blog-block"><p>Sem a ajuda de pessoas como você, nosso trabalho não seria possível. O Greenpeace Brasil é uma organização independente - não aceitamos recursos de empresas, governos ou partidos políticos. Por favor, faça uma doação hoje mesmo e nos ajude a ampliar nosso trabalho de pesquisa, monitoramento e denúncia de crimes ambientais. Clique abaixo e faça a diferença!</p><div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-2"><div class="wp-block-button"><a href="https://doe.greenpeace.com.br/?cc=701Pm00000T8VcFIAV&entrypoint=blog_donate" class="wp-block-button__link wp-element-button">Doe agora</a></div></div></div>
         <style>
         .gpbr-custom-blog-block p {
             padding-top: 2em !important;
