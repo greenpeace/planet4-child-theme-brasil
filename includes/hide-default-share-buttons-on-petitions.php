44e2b43cc3237f1b77bb485542d798f1cf549ca5
Rickson Figueira <sadrisco@outlook.com>
2025-04-04 14:55:59 -0300

    Feat: Hides native share buttons in petition share message
    Added logic to hide native share buttons in the petition share message.

---
+++ includes/hide-default-share-buttons-on-petitions.php
@@ -0,0 +1,8 @@
+<?php
+
+function hide_default_share_buttons_on_petitions() {
+    if (strpos($_SERVER['REQUEST_URI'], '/apoie/') !== false) {
+        echo '<style>.share-buttons { display: none !important; }</style>';
+    }
+}
+add_action('wp_head', 'hide_default_share_buttons_on_petitions');
