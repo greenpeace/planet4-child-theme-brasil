573c574eb646118312600fad7fd1383ffffaf8ce
Rickson Figueira <sadrisco@outlook.com>
2025-05-28 09:14:57 -0300

    Released convert in dev

--- functions.php
+++ functions.php
@@ -17,6 +17,8 @@ require_once( 'includes/audima.php' );
 require_once( 'includes/reading-time-string-translation.php' );
 require_once ('includes/add-custom-block-after-all-blog-posts.php');
 require_once ('includes/hide-default-share-buttons-on-petitions.php');
+require_once ('includes/convert-scripts.php');
+
 
 /**
  * Functions not in use
