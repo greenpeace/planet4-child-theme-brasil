b28ffb9d5244e1ee9b1460ff285347bf3a551b8b
Rickson Figueira <sadrisco@outlook.com>
2025-06-13 16:08:59 -0300

    Added convert scripts for prod and dev

--- includes/convert-scripts.php
+++ includes/convert-scripts.php
@@ -1,10 +1,21 @@
 <?php
 function inject_convert_script() {
-    ?>
-<!-- begin Convert Experiences code-->
- <script data-cfasync="false" type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=staging"></script>
- <!-- end Convert Experiences code -->
-    <?php
+    // Pega a URL atual completa
+    $current_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    
+    // Verifica se a URL contém 'www-dev'
+    if (strpos($current_url, 'www-dev') !== false) {
+        // Script para ambiente com 'www-dev'
+        ?>
+        <!-- begin Convert Experiences code (staging) -->
+        <script data-cfasync="false" type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=staging"></script>
+        <!-- end Convert Experiences code -->
+        <?php
+    } else {
+        // Script para ambiente sem 'www-dev'
+        ?>
+        <!-- begin Convert Experiences code--><script type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=production"></script><!-- end Convert Experiences code -->
+        <?php
+    }
 }
-add_action('wp_head', 'inject_convert_script', 5); // Prioridade 5 para carregar logo após <title>
-
+add_action('wp_head', 'inject_convert_script', 1); // Prioridade 1 para carregar no topo
