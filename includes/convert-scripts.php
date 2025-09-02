a0a3957fde2329fe0c7701a6a511a97dda003316
Rickson Figueira <sadrisco@outlook.com>
2025-09-02 16:00:46 -0300

    Feat: Added Mixpanel Datalayer for convert.com

--- includes/convert-scripts.php
+++ includes/convert-scripts.php
@@ -9,13 +9,64 @@ function inject_convert_script() {
         ?>
         <!-- begin Convert Experiences code (staging) -->
         <script data-cfasync="false" type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=staging"></script>
+        <script>
+            var refObject = window['convert']['data']['experiments']
+    	    for (var key in window["convert"]["currentData"]["experiments"]) {
+		    if (!window["convert"]["currentData"]["experiments"].hasOwnProperty(key)) {
+		    	continue;
+		    }}
+            
+    	    var currentExperiment = window["convert"]["currentData"]["experiments"][key];
+	        var curExperimentName = refObject[key] && refObject[key].n ? refObject[key].n : "unknown experiment name";
+	        curExperimentName = curExperimentName.replace("Test #", "Test ");
+	        var curVariant = currentExperiment['variation_name'] ? currentExperiment['variation_name'] : "unknown variant";
+	        curVariant = curVariant.replace("Var #", "Variation ");
+	        mixpanel.track('Convert_Experiences', {'Experiment name': curExperimentName, 'Variant name': curVariant})
+        </script>
         <!-- end Convert Experiences code -->
         <?php
-    } else {
+    } else if (strpos($current_url, 'planet4.test') !== false){
         // Script para ambiente sem 'www-dev'
         ?>
-        <!-- begin Convert Experiences code--><script type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=production"></script><!-- end Convert Experiences code -->
+        <!-- begin Convert Experiences code--><script type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=staging"></script><!-- end Convert Experiences code -->
+        <script>
+            var refObject = window['convert']['data']['experiments']
+    	    for (var key in window["convert"]["currentData"]["experiments"]) {
+		    if (!window["convert"]["currentData"]["experiments"].hasOwnProperty(key)) {
+		    	continue;
+		    }}
+            
+    	    var currentExperiment = window["convert"]["currentData"]["experiments"][key];
+	        var curExperimentName = refObject[key] && refObject[key].n ? refObject[key].n : "unknown experiment name";
+	        curExperimentName = curExperimentName.replace("Test #", "Test ");
+	        var curVariant = currentExperiment['variation_name'] ? currentExperiment['variation_name'] : "unknown variant";
+	        curVariant = curVariant.replace("Var #", "Variation ");
+	        mixpanel.track('Convert_Experiences', {'Experiment name': curExperimentName, 'Variant name': curVariant})
+        </script>
+
         <?php
+    }
+
+    else {
+                ?>
+        <!-- begin Convert Experiences code--><script type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/100414404-100415998.js?environment=production"></script><!-- end Convert Experiences code -->
+        <script>
+            var refObject = window['convert']['data']['experiments']
+    	    for (var key in window["convert"]["currentData"]["experiments"]) {
+		    if (!window["convert"]["currentData"]["experiments"].hasOwnProperty(key)) {
+		    	continue;
+		    }}
+            
+    	    var currentExperiment = window["convert"]["currentData"]["experiments"][key];
+	        var curExperimentName = refObject[key] && refObject[key].n ? refObject[key].n : "unknown experiment name";
+	        curExperimentName = curExperimentName.replace("Test #", "Test ");
+	        var curVariant = currentExperiment['variation_name'] ? currentExperiment['variation_name'] : "unknown variant";
+	        curVariant = curVariant.replace("Var #", "Variation ");
+	        mixpanel.track('Convert_Experiences', {'Experiment name': curExperimentName, 'Variant name': curVariant})
+        </script>
+
+        <?php
+
     }
 }
 add_action('wp_head', 'inject_convert_script', 1); // Prioridade 1 para carregar no topo
