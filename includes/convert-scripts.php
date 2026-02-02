4fb4de9b52a570bf61129cab9c83e617f19bc60c
Rickson Figueira <sadrisco@outlook.com>
2026-02-02 14:40:43 -0300

    (re) Added Convert.com main script

--- includes/convert-scripts.php
+++ includes/convert-scripts.php
@@ -18,23 +18,3 @@
     }
   
     add_action('wp_head', 'inject_convert_script', 1); // Prioridade 1 para carregar no topo
-
-    function inject_convert_mixpanel_datalayer() {
-        ?>
-        <script>
-            var refObject = window['convert']['data']['experiments']
-    	    for (var key in window["convert"]["currentData"]["experiments"]) {
-		    if (!window["convert"]["currentData"]["experiments"].hasOwnProperty(key)) {
-		    	continue;
-		    }}
-    	    var currentExperiment = window["convert"]["currentData"]["experiments"][key];
-	        var curExperimentName = refObject[key] && refObject[key].n ? refObject[key].n : "unknown experiment name";
-	        curExperimentName = curExperimentName.replace("Test #", "Test ");
-	        var curVariant = currentExperiment['variation_name'] ? currentExperiment['variation_name'] : "unknown variant";
-	        curVariant = curVariant.replace("Var #", "Variation ");
-	        mixpanel.track('Convert_Experiences', {'Experiment name': curExperimentName, 'Variant name': curVariant})
-        </script>
-        <?php
-    }
-
-    add_action('wp_footer', 'inject_convert_mixpanel_datalayer', 100); // Prioridade 100 para carregar bem abaixo
