6fd69734b550ed05626806cadf9924af3e967e09
Rickson Figueira <sadrisco@outlook.com>
2026-02-03 11:43:09 -0300

    Revert "(re) Added Convert.com main script"
    This reverts commit 4fb4de9b52a570bf61129cab9c83e617f19bc60c.

--- includes/gravity-forms/brazilian-phone-mask.php
+++ includes/gravity-forms/brazilian-phone-mask.php
@@ -1,16 +1,17 @@
 <?php
 /*
- * Creates the brazilian phone mask for phone inputs in international format
+ * Creates the brazilian phone mask for phone inputs
  */ 
-function gpbr_add_brazilian_phone_mask_gf($phone_formats)
-{
-    $phone_formats['br'] = array(
-        'label' => 'Brasileiro com 9 dígitos e DDD (Internacional)',
-        'mask' => '+55-99-99999-9999',
-        'regex' => '/^\+55(1[1-9]|2[12478]|3[1234578]|4[1-9]|5[1345]|6[1-9]|7[134579]|8[1-9]|9[1-9])[9][0-9]{8}$/',
-        'error_message_setting' => 'Houve um erro com a submissão, verifique o campo em destaque. O telefone deve estar no formato +5511999999999 com um DDD válido e 9 dígitos no número.'
-    );
-    return $phone_formats;
-}
 
-add_filter('gform_phone_formats', 'gpbr_add_brazilian_phone_mask_gf', 10, 2);
+ function gpbr_add_brazilian_phone_mask_gf($phone_formats)
+ {
+     $phone_formats['br'] = array(
+         'label' => 'Brasileiro com 9 dígitos e DDD',
+         'mask' => '99-99999-9999',
+         'regex' => '/^(1[1-9]|2[12478]|3[1234578]|4[1-9]|5[1345]|6[1-9]|7[134579]|8[1-9]|9[1-9])-[9][0-9]{4}-[0-9]{4}$/',
+         'error_message_setting' => 'Houve um erro com a submissão, verifique o campo em destaque. O telefone deve conter um DDD válido e ser no formato celular brasileiro.'
+        );
+     return $phone_formats;
+ }
+ 
+ add_filter('gform_phone_formats', 'gpbr_add_brazilian_phone_mask_gf', 10, 2);
